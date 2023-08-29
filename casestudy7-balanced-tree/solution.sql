c171d7c7dfa5c4a3545a8fd93aea09f412f45b59
sheela-padhy <139641763+sheela-padhy@users.noreply.github.com>
2023-08-29 08:16:50 +0530

    PART C ADDED

--- casestudy7-balanced-tree/solution.sql
+++ casestudy7-balanced-tree/solution.sql
@@ -34,3 +34,53 @@ select case when member=0 then 'male' else 'female' end as gender,percentage fro
 
 # What is the average revenue for member transactions and non-member transactions?
 select member,sum(qty*price) as revenue from sales group by member;
+
+
+# C.Product Analysis
+  
+# What are the top 3 products by total revenue before discount?
+
+with revenue_cte as 
+(select product_name,sum(s.qty*s.price) as total_revenue from sales as s inner join product_details as d on s.prod_id=d.product_id group by product_name)
+,rank_cte as 
+(select product_name,rank() over(order by total_revenue) as revenue from revenue_cte)
+select product_name,revenue from rank_cte where revenue <=3;
+
+# What is the total quantity, revenue and discount for each segment?
+
+select  p.segment_name,sum(qty) as total_Quantity,sum(s.qty*s.price) as total_revenue ,sum((s.qty*s.price*s.discount))/100 as discount from sales as s inner join product_details as 
+p on p.product_id=s.prod_id group by p.segment_name  order by total_revenue desc ;
+
+
+# What is the top selling product for each segment?
+
+select product_name,segment_name from product_details;
+with product_cte as 
+(select segment_name ,product_name,rank() over(partition by segment_name order by sum(qty) desc) as quantity_rank from sales as s inner join product_details as p  on p.product_id=s.prod_id  group by segment_name,product_name)
+select segment_name ,product_name from product_cte where quantity_rank = 1; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
